04c7a66b790612288804d3d47a99ce519b89eb2b
temarych <teemest06@icloud.com>
2023-10-27 10:54:09 +0300

    Fix

--- src/middleware/handleError.ts
+++ src/middleware/handleError.ts
@@ -3,16 +3,25 @@ import {
   type Request,
   type Response
 }                         from 'express';
-import { type HttpError } from '../models/error';
+import { HttpError } from '../models/error';
 
 export const handleError = (
-  error   : HttpError,
+  error   : Error,
   request : Request,
   response: Response,
   next    : NextFunction
 ) => {
-  response.status(error.status).send({
-    code : error.code,
+  if (error instanceof HttpError) {
+    return response.status(error.status).send({
+      code : error.code,
+      error: error.message
+    });
+  }
+
+  console.error(error);
+
+  response.status(500).send({
+    code : 500,
     error: error.message
   });
 };
