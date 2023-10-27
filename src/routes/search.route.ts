2ac64d5c3dfb9007bd8134f6ca627179e4ac04e7
temarych <teemest06@icloud.com>
2023-10-27 12:05:03 +0300

    Update rights

--- src/routes/search.route.ts
+++ src/routes/search.route.ts
@@ -1,6 +1,7 @@
 import { Router }       from 'express';
 import { searchGuides } from '../controllers/search.controller';
+import { authorize }    from '../middleware/authorize';
 
 export const searchRoute = Router();
 
-searchRoute.get('/guides', searchGuides);
+searchRoute.get('/guides', authorize, searchGuides);
