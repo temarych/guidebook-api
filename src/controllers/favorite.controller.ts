559c4014b4fa070d987c229e97a47c73fa8241b6
temarych <teemest06@icloud.com>
2023-10-27 19:21:05 +0300

    Add removal logic

--- src/controllers/favorite.controller.ts
+++ src/controllers/favorite.controller.ts
@@ -29,6 +29,30 @@ export const addFavoriteGuide = async (request: Request, response: Response) => 
   });
 };
 
+export const removeFavoriteGuide = async (request: Request, response: Response) => {
+  const user            = request.user as User;
+  const { id: guideId } = request.params;
+
+  const favorite = await prisma.favorite.findFirst({
+    where: { guideId, userId: user.id }
+  });
+
+  if (favorite === null) {
+    return response.status(400).send({
+      code   : 'not-favorite',
+      message: 'Guide is not favorite'
+    });
+  }
+
+  await prisma.favorite.delete({
+    where: { id: favorite.id }
+  });
+
+  response.send({
+    message: 'Removed guide from favorites'
+  });
+};
+
 export const getFavoriteGuides = async (request: Request, response: Response) => {
   const user = request.user as User;
 
--- src/routes/favorite.route.ts
+++ src/routes/favorite.route.ts
@@ -2,10 +2,12 @@ import { Router }    from 'express';
 import { authorize } from '../middleware/authorize';
 import {
   addFavoriteGuide,
-  getFavoriteGuides
+  getFavoriteGuides,
+  removeFavoriteGuide
 }                    from '../controllers/favorite.controller';
 
 export const favoriteRoute = Router();
 
 favoriteRoute.put('/:id', authorize, addFavoriteGuide);
+favoriteRoute.delete('/:id', authorize, removeFavoriteGuide);
 favoriteRoute.get('/', authorize, getFavoriteGuides);
