871795b16389e899460b16e9b9c9c6383cecb2dc
temarych <teemest06@icloud.com>
2023-10-28 11:12:05 +0300

    Add user deletion logic

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
-import { Router }                     from 'express';
-import { getMe, signIn, signUp }      from '../controllers/auth.controller';
-import { validate }                   from '../middleware/validate';
-import { authorize }                  from '../middleware/authorize';
-import { signInSchema, signUpSchema } from '../schemas/auth.schema';
+import { Router }                          from 'express';
+import { deleteMe, getMe, signIn, signUp } from '../controllers/auth.controller';
+import { validate }                        from '../middleware/validate';
+import { authorize }                       from '../middleware/authorize';
+import { signInSchema, signUpSchema }      from '../schemas/auth.schema';
 
 export const authRoute = Router();
 
 authRoute.post('/signup', validate(signUpSchema), signUp);
 authRoute.post('/signin', validate(signInSchema), signIn);
 authRoute.get('/me', authorize, getMe);
+authRoute.delete('/me', authorize, deleteMe);
